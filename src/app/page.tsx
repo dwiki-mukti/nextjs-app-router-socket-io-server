83bdaaf135ee76f21995073c645415fb7fd5729a
dwiki-mukti <243dwiki.com@gmail.com>
2025-03-22 00:12:01 +0700

    Create project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,77 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import React, { useEffect, useState } from 'react';
+import { io, Socket } from "socket.io-client";
+
+export const socket = (() => {
+  const socketHost = undefined;
+  const path = '/api/socket/init-server';
+  if (typeof window == "undefined") return io(socketHost, { path });
+  if (!(window as any)._socket) {
+    (window as any)._socket = io(socketHost, { path });
+  }
+  return (window as any)._socket as Socket;
+})();
+
+export default function Page() {
+  const UserAuthed = { id: 1 }
+  const [ListNotif, setListNotif] = useState<{
+    user_id: number;
+    url: string;
+    description: string;
+    created_at: string;
+    status: "READ" | "UNREAD";
+  }[]>([]);
+  const apps: Record<string, any> = { "E-Office": 2, "E-Procurement": 3 };
+
+
+
+  /**
+   * Function handler
+   */
+  function onSendNotif(data: any) {
+    console.log(data);
+    setListNotif((prev) => ([data, ...prev]));
+  };
+
+
+
+  /**
+   * Use effect
+   */
+  useEffect(() => {
+    fetch('/api/socket/init-server');
+    socket.on("s_SendNotif", onSendNotif);
+
+    return () => {
+      socket.off("s_SendNotif", onSendNotif);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (UserAuthed.id) {
+      socket.emit("c_SubscribeNotif", { user_id: UserAuthed?.id });
+    }
+  }, [UserAuthed])
+
+
+
+  /**
+   * Render JSX
+   */
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <section className="relative z-10 overflow-hidden px-6 pb-16 pt-10 md:pb-20 lg:pb-28 lg:pt-[140px]">
+      <div className="mx-auto max-w-full pt-16">
+        <div>ini notif:</div>
+        <div>
+          {ListNotif.map((notification, indexNotification) => (
+            <div key={indexNotification} className={'px-4 py-3 cursor-pointer'}>
+              <div dangerouslySetInnerHTML={{ __html: notification.description ?? '' }} />
+              <div className='mt-2 text-sm text-blue-500'>{notification.created_at}</div>
+            </div>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+    </section>
+  )
 }
